daa0069f0cc2056bd4038b7b1a66c50f679720dc
james <50460460+alexacallmebaka@users.noreply.github.com>
2023-11-04 20:22:38 -0500

    message listener

---
+++ listener.py
@@ -0,0 +1,42 @@
+#message listener
+
+import redis
+
+import json
+
+import logging
+
+import message as msg
+
+class Listener:
+    """Listener for messages in a chat room."""
+    
+    def __init__(self, room: str,  port: int = 6379, host: str = 'localhost', debug: bool = False) -> None:
+        self.room: str = room
+
+        self.debug = debug
+
+        self.server : redis.Redis.PubSub = redis.Redis(host=host, port=port, decode_responses=True).pubsub()
+
+        self.server.subscribe(room)
+        
+        if self.debug:
+            logging.basicConfig(level=logging.DEBUG)
+            logging.debug(f'Debug logging active.')
+
+        while True:
+            stream_data = self.server.get_message()
+
+            #need to check if data exists and if it is a string.
+            if stream_data and type(stream_data['data']) == str:
+                try:
+                    print(msg.Message.deserialize(stream_data['data']))
+                    if self.debug:
+                        logging.debug(f'Message recieved: \"{stream_data["data"]}\"')
+
+                #just ignore bad message, output in debug log
+                except json.JSONDecodeError:
+                    logging.debug(f'[ERROR] Bad message recieved: \"{stream_data["data"]}\"')
+
+
+
